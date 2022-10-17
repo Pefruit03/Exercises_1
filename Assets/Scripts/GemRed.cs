22e99e148f865f10dc0c947eec293c2e18b04de6
Pefruit03 <pegerken@gmail.com>
2022-10-17 14:59:21 +0700

    Unity3D

---
+++ Assets/Scripts/GemRed.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GemRed : MonoBehaviour
+{
+    public GameObject scoreBox;
+    public AudioSource collectSound;
+
+    void OnTriggerEnter()
+    {
+        GlobalScore.currentScore += 200;
+        collectSound.Play();
+        Destroy(gameObject);
+    }
+}
